e1fe8ea7cd3142bb2244aa82cdb7ddb039418122
Harry <hrgus3@gmail.com>
2022-03-17 13:31:59 -0400

    added h1's to all components, and added functionality to Movies.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,31 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(movies)
+  
+  
+  const movieGenres = movies.map((movie) => {
+    return (
+      <li key={movie.title}>{movie.genres}</li>
+    
+    );
+  })
+
+  const allMovies = movies.map((movie) => {
+    return (
+      <div>
+        {movie.title + "," + " " + movie.time}
+        <ul key={movie.title}>{movieGenres}</ul>
+      </div>
+    )
+  })
+  
+  return (
+    <div>
+      <h1>Movies Page</h1> 
+      {allMovies}
+    </div>
+  );
 }
 
 export default Movies;
