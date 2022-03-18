03a7b6a4e7974c52e127ddd1dcc69ea97523692b
Harry Gustafson <hrgus3@gmail.com>
2022-03-18 03:11:21 -0400

    ran into issue with Directors.js, fixed bug in Home.js

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -3,10 +3,20 @@ import { directors } from "../data";
 
 function Directors() {
   
+  const allDirectors = directors.map((director) => {
+    let directorsMovies = directors.movies.map((movie) => <li>{movie}</li>)
+    return (
+      <div>
+        {director.name}
+        <ul>{directorsMovies}</ul>
+      </div>
+    )
+  })
   
   return (
     <div>
       <h1>Directors Page</h1>
+      {allDirectors}
     </div>
   );
 }
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,21 +2,15 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  console.log(movies)
-  
-  
-  const movieGenres = movies.map((movie) => {
-    return (
-      <li key={movie.title}>{movie.genres}</li>
-    
-    );
-  })
 
   const allMovies = movies.map((movie) => {
+    let movieGenre = movie.genres.map((genre) => <li>{genre}</li>)
     return (
       <div>
-        {movie.title + "," + " " + movie.time}
-        <ul key={movie.title}>{movieGenres}</ul>
+        {`${movie.title}, ${movie.time}`}
+        <ul key={movie.title}>
+          {movieGenre}
+        </ul>
       </div>
     )
   })
